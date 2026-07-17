6256eb3351ed9f37cf701fad25151528878d26fe
Lakhan Dhakne <lakhandhakne119@gmail.com>
2026-07-17 13:33:07 +0530

    SCRUM-6: Implement JWT authentication with Register/Login, protect Employee endpoints

--- Lucky/Controllers/EmployeeController.cs
+++ Lucky/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Lucky.Data;
 using Lucky.Models;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,7 @@ namespace Lucky.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class EmployeeController : ControllerBase
     {
         private readonly AppDbContext _context;
